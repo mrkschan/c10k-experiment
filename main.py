476572a78e8cbae7ad27ec8d56521ead190448fa
KS Chan <mrkschan@gmail.com>
2013-07-24 16:06:35 +0800

    Included a server impl from socket example

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import socket
+
+
 def main():
-    pass
+    # Ref: http://is.gd/S1dtCH
+    HOST, PORT = '127.0.0.1', 8000
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+
+    s.listen(0)
+
+    conn, addr = s.accept()
+    print 'Connected: %(addr)s' % {'addr': addr}
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+
+        conn.sendall(data)
+        conn.close()
 
 
 if __name__ == '__main__':
