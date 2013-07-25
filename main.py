5e84cc4fc236a52d6269ad22d2ca2e38c4ca5730
KS Chan <mrkschan@gmail.com>
2013-07-25 09:32:09 +0800

    Use multiprocesses to handle multiple clients

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
+import multiprocessing
+import os
 import socket
+
+
+def handle_conn(conn, addr):
+    msg = 'Process: %(pid)s; Client: %(addr)s' % {
+        'pid': os.getpid(),
+        'addr': addr,
+    }
+    print msg
+
+    data = conn.recv(32)
+    conn.sendall(data)
+    conn.close()
 
 
 def main():
@@ -8,17 +22,19 @@ def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT))
 
-    s.listen(0)
-
-    conn, addr = s.accept()
-    print 'Connected: %(addr)s' % {'addr': addr}
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-
-        conn.sendall(data)
-    conn.close()
+    child = []
+    try:
+        s.listen(0)
+        while True:
+            conn, addr = s.accept()
+            p = multiprocessing.Process(target=handle_conn, args=(conn, addr))
+            p.start()
+            child.append(p)
+    except:
+        pass
+    finally:
+        [p.terminate() for p in child if p.is_alive()]
+        s.close()
 
 
 if __name__ == '__main__':
