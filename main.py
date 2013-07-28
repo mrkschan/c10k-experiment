2eda3e9f1ce74cf6e8fb297b16acbc7507dde479
KS Chan <mrkschan@gmail.com>
2013-07-28 21:06:30 +0800

    Added trials fornon-blocking accept() with single-/multi-process blocking recv()

--- main.py
+++ main.py
@@ -11,20 +11,19 @@ def handle_conn(conn, addr):
     }
     print msg
 
+    print 'Wait for data from peer'
     data = conn.recv(32)
+
+    print 'Data received from peer'
     conn.sendall(data)
     conn.close()
 
 
-def select_server(socket_):
+def basic_server(socket_):
     child = []
     try:
         while True:
             print 'Waiting for peer'
-            readable, w, e = select.select([socket_], [], [], 1)
-
-            if socket_ not in readable:
-                continue
 
             conn, addr = socket_.accept()
             print 'Peer connected:', addr
@@ -36,11 +35,42 @@ def select_server(socket_):
         [p.terminate() for p in child if p.is_alive()]
 
 
-def basic_server(socket_):
+def select_server_v0(socket_):
+    '''Single process select(). Use non-blocking accept() but blocking recv().
+
+    Since this is using single process and recv() blocks the next accept(),
+    concurrency is not achieved.
+    '''
+    while True:
+        print 'Waiting for peer'
+        readable, w, e = select.select([socket_], [], [], 1)
+
+        if socket_ not in readable:
+            continue
+
+        conn, addr = socket_.accept()
+        print 'Peer connected:', addr
+
+        handle_conn(conn, addr)
+
+
+def select_server_v1(socket_):
+    '''Single process select() with sub-processes to recv(). Use non-blocking
+       accept() but blocking recv().
+
+    Non-blocking accept() is not affected by the blocking recv() since the
+    blocks happens in the sub-processes. Concurrency is achieved but it's only
+    a slight improvement from *basic_server* because CPU time is still consumed
+    for context-switching when waiting for the blocking recv().
+    '''
     child = []
     try:
         while True:
             print 'Waiting for peer'
+            readable, w, e = select.select([socket_], [], [], 1)
+
+            if socket_ not in readable:
+                continue
 
             conn, addr = socket_.accept()
             print 'Peer connected:', addr
@@ -64,9 +94,13 @@ def main():
         #socket_.listen(0)
         #basic_server(socket_)
 
+        #socket_.setblocking(0)
+        #socket_.listen(0)
+        #select_server_v0(socket_)
+
         socket_.setblocking(0)
         socket_.listen(0)
-        select_server(socket_)
+        select_server_v1(socket_)
     finally:
         socket_.close()
 
